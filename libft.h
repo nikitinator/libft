7778b480b7f3cd1ad7d08d368a619ca211bbd37e
Serhiy Nikitin <serhiy.nikitin@lotusflare.com>
2019-07-19 21:01:19 +0300

    add clarifying newlines in libft.h

--- libft.h
+++ libft.h
@@ -35,6 +35,7 @@ void				*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void				*ft_memmove(void *dst, const void *src, size_t len);
 void				*ft_memchr(const void *s, int c, size_t n);
 int					ft_memcmp(const void *s1, const void *s2, size_t n);
+
 size_t				ft_strlen(const char *s);
 char				*ft_strdup(const char *s1);
 char				*ft_strcpy(char *dst, const char *src);
@@ -49,30 +50,40 @@ char				*ft_strnstr(const char *big,
 		const char *little, size_t len);
 int					ft_strcmp(const char *s1, const char *s2);
 int					ft_strncmp(const char *s1, const char *s2, size_t n);
+
 int					ft_atoi(const char *str);
+int					ft_atoi_base(char *str, int base);
+
 int					ft_isprint(int c);
 int					ft_isalpha(int c);
 int					ft_isdigit(int c);
 int					ft_isascii(int c);
 int					ft_isalnum(int c);
+
 int					ft_tolower(int c);
 int					ft_toupper(int c);
+
 void				*ft_memalloc(size_t size);
 void				ft_memdel(void **ap);
+
 char				*ft_strnew(size_t size);
 void				ft_strdel(char **as);
 void				ft_strclr(char *s);
 void				ft_striter(char *s, void (*f)(char*));
 void				ft_striteri(char *s, void (*f)(unsigned int, char *));
+
 char				*ft_strmap(char const *s, char (*f)(char));
 char				*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+
 int					ft_strequ(char const *s1, char const *s2);
 int					ft_strnequ(char const *s1, char const *s2, size_t n);
 char				*ft_strsub(char const *s, unsigned int start, size_t len);
 char				*ft_strjoin(char const *s1, char const *s2);
 char				*ft_strtrim(char const *s);
 char				**ft_strsplit(char const *s, char c);
+
 char				*ft_itoa(int n);
+
 void				ft_putchar(char c);
 void				ft_putstr(char const *s);
 void				ft_putendl(char const *s);
@@ -81,6 +92,7 @@ void				ft_putchar_fd(char c, int fd);
 void				ft_putstr_fd(char const *s, int fd);
 void				ft_putendl_fd(char const *s, int fd);
 void				ft_putnbr_fd(int n, int fd);
+
 t_list				*ft_lstnew(void const *content, size_t content_size);
 void				ft_lstdelone(t_list **alst, void (*del)(void*, size_t));
 void				ft_lstdel(t_list **alsr, void (*del)(void*, size_t));
@@ -100,7 +112,6 @@ size_t				ft_cntwrd(char *str);
 size_t				ft_cntchr(char *str, char k);
 int					ft_iswhsp(int c);
 char				*ft_strjoin_free(char const *s1, char const *s2);
-int					ft_atoi_base(char *str, int base);
 size_t				ft_list_count(t_list *begin_list);
 void				ft_swap_int(int *a, int *b);
 
